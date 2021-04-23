b8b18dcc3b6ecd622c9714b5b778f7a2df63c705
anujakumbhare <83079269+anujakumbhare@users.noreply.github.com>
2021-04-23 20:41:52 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileReadWrite;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3rd: 
+            FileReader.ReadFile();
+            FileReader.WriteToFile();
+
+            //2nd:
+            /*int a = 20;
+            int b = 29;
+            int total=a+b;
+            Console.Write("Total {0}+{1} = {2}", a, b, total);
+            Console.Read();
+            */
+
+            //1st:
+            // Console.WriteLine("Hello...My name is Anuja Kumbhare and " + args[0] + " " +args[1]);
+            // Console.Read();
+        }
+    }
+}
