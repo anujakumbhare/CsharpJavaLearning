8cb7f99069081921043b716040920f5b8828c769
anujakumbhare <83079269+anujakumbhare@users.noreply.github.com>
2021-04-23 20:26:22 +0530

    Add files via upload
    Read and Write to a text file

---
+++ FileReadWrite/FileReadWrite/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReadWrite
+{
+    public static class FileReader
+    {
+        public static void ReadFile()
+        {
+            String line;
+            StreamReader sr = new StreamReader("C:\\Users\\Anuja\\Desktop\\SampleFile.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+           // Console.ReadLine();
+            return;
+        }
+
+        public static void WriteToFile()
+        {
+            StreamWriter sw = new StreamWriter("C:\\Users\\Anuja\\Desktop\\SampleFile1.txt");
+
+             String line;
+             StreamReader sr = new StreamReader("C:\\Users\\Anuja\\Desktop\\SampleFile.txt");
+             line = sr.ReadLine();
+             while (line != null)
+             {
+                 Console.WriteLine(line);
+                 sw.WriteLine(line + " Hello world");
+                 line = sr.ReadLine();
+             }
+             sr.Close();
+            //sw.WriteLine(" Hello world");
+            sw.Close();
+            return;
+        }
+    }
+}
